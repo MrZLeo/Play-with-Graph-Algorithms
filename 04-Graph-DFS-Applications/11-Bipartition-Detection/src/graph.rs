5d7d07f243071a485202f7e44adc5d379e218488
mrzleo <mr.zleo10@gmail.com>
2022-01-27 19:14:24 +0800

    chapter04-11

---
+++ 04-Graph-DFS-Applications/11-Bipartition-Detection/src/graph.rs
@@ -0,0 +1,102 @@
+use core::panic;
+use std::collections::hash_set::Iter;
+use std::collections::HashSet;
+use std::fmt::{Display, Formatter};
+use std::fs;
+
+#[derive(Debug)]
+pub struct Graph {
+    v: usize,
+    e: usize,
+    adj: Vec<HashSet<usize>>,
+}
+
+impl Graph {
+    pub fn from(filename: &str) -> Self {
+        let file = fs::read_to_string(filename).unwrap();
+        let mut iter = file.split("\n").into_iter();
+        let mut first_line = iter.next().unwrap().split_whitespace();
+
+        let v = first_line.next().unwrap().parse::<usize>().unwrap();
+        let e = first_line.next().unwrap().parse::<usize>().unwrap();
+
+        let mut ret = Graph {
+            v,
+            e,
+            adj: vec![HashSet::new(); v as usize],
+        };
+
+        while let Some(line) = iter.next() {
+            if line.len() == 0 {
+                break;
+            }
+            let mut line = line.split_whitespace();
+            let x = line.next().unwrap().parse::<usize>().unwrap();
+            let y = line.next().unwrap().parse::<usize>().unwrap();
+            ret.validate_vertex(x);
+            ret.validate_vertex(y);
+
+            if x == y {
+                panic!("Self Loop is Detected!");
+            }
+            if ret.adj[x].contains(&y) {
+                panic!("Parallel Edges are Detected!");
+            }
+
+            ret.adj[x].insert(y);
+            ret.adj[y].insert(x);
+        }
+
+        return ret;
+    }
+
+    pub fn e(&self) -> usize {
+        self.e
+    }
+
+    pub fn v(&self) -> usize {
+        self.v
+    }
+
+    pub fn has_edge(&self, v1: usize, v2: usize) -> bool {
+        self.validate_vertex(v1);
+        self.validate_vertex(v2);
+        return self.adj[v1].contains(&v2);
+    }
+
+    fn validate_vertex(&self, v: usize) {
+        if v >= self.v as usize {
+            panic!("vertex {} is not valid", v);
+        }
+    }
+
+    pub fn adj_edge(&self, v: usize) -> Iter<'_, usize> {
+        self.validate_vertex(v);
+
+        self.adj[v].iter()
+    }
+
+    pub fn degree(&self, v: usize) -> usize {
+        self.validate_vertex(v);
+
+        self.adj[v].len()
+    }
+}
+
+impl Display for Graph {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let mut list = String::new();
+        for v in 0..self.v {
+            list.push_str(format!("{}: ", v).as_str());
+            for w in &self.adj[v] {
+                list.push_str(format!("{} ", w).as_str());
+            }
+            list.push('\n');
+        }
+        write!(
+            f,
+            "AdjList: \nV: {}, E: {}\nList: \n{}",
+            self.v, self.e, list
+        )
+    }
+}
---
+++ 04-Graph-DFS-Applications/11-Bipartition-Detection/src/main.rs
@@ -0,0 +1,52 @@
+mod graph;
+
+mod bipartition_detection {
+
+    use crate::graph::Graph;
+
+    pub fn is_bipartition(g: &Graph) -> bool {
+        let mut visited = vec![false; g.v()];
+        let mut colors = vec![-1; g.v()];
+        for v in 0..g.v() {
+            if visited[v] == false {
+                if __dfs(&g, v, &mut visited, &mut colors, 0) == false {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    fn __dfs(g: &Graph, v: usize, visited: &mut Vec<bool>, colors: &mut Vec<i32>, color: i32) -> bool {
+        visited[v] = true;
+        colors[v] = color;
+
+        for &w in g.adj_edge(v) {
+            if visited[w] == false {
+                if __dfs(g, w, visited, colors, 1 - color) == false {
+                    return false;
+                }
+            } else if colors[w] == colors[v] {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+use crate::{bipartition_detection::is_bipartition, graph::Graph};
+
+pub fn main() {}
+
+#[test]
+pub fn cycle_detection_test() {
+    let g = Graph::from("g.txt");
+    let g2 = Graph::from("g2.txt");
+    let g3 = Graph::from("g3.txt");
+
+    assert_eq!(is_bipartition(&g), true);
+    assert_eq!(is_bipartition(&g2), false);
+    assert_eq!(is_bipartition(&g3), true);
+}
