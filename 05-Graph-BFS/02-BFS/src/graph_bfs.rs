ab0b0506a7d4f5c228474213c9fb03f99e47109b
mrzleo <mr.zleo10@gmail.com>
2022-07-25 15:01:33 +0800

    fix bug of bfs

--- 05-Graph-BFS/02-BFS/src/graph_bfs.rs
+++ 05-Graph-BFS/02-BFS/src/graph_bfs.rs
@@ -28,21 +28,38 @@ impl GraphBFS {
 }
 
 impl Bfs for GraphBFS {
-    fn bfs(&mut self, s: usize) -> Box<dyn Iterator<Item = usize>> {
-        let mut q = VecDeque::new();
-        q.push_back(s);
-        self.visited[s] = true;
+    fn bfs(&mut self, source: usize) -> Box<dyn Iterator<Item = usize>> {
+        // inner bfs, start from s
+        let mut bfs_inner = |s| {
+            // check if `s` is visited before
+            // MUST do this in `bfs_inner` because we have mutable access inside
+            // closure, so we can't have a immutable access outside
+            if self.visited[s] {
+                return;
+            }
 
-        while !q.is_empty() {
-            let v = q.pop_front().unwrap();
-            // println!("{v}");
-            self.res.push(v);
-            for next in self.graph.adj_edge(v) {
-                if !self.visited[next] {
-                    q.push_back(next);
-                    self.visited[next] = true;
+            let mut q = VecDeque::new();
+            q.push_back(s);
+            self.visited[s] = true;
+
+            while !q.is_empty() {
+                let v = q.pop_front().unwrap();
+                self.res.push(v);
+                for next in self.graph.adj_edge(v) {
+                    if !self.visited[next] {
+                        q.push_back(next);
+                        self.visited[next] = true;
+                    }
                 }
             }
+        };
+
+        // we should search first from s
+        // and if there's some v we can't reach, search latter
+        bfs_inner(source);
+
+        for v in 0..self.graph.v() {
+            bfs_inner(v);
         }
 
         Box::new(self.res.clone().into_iter())
